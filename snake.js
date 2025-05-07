c87675027de9297b8be3417e109d5cfe41792515
tonybaloneywow <nguyentony2028@gmail.com>
2025-05-07 17:52:27 -0400

    may 7 (snake)

---
+++ snake.js
@@ -0,0 +1,41 @@
+
+//board
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+//snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+//food
+var  foodX;
+var foodY;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    update();
+}
+
+function update() {
+    context.fillStyle="black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle="lime";
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    context.fillStyle="red"
+    context.fillRect(foodX, foodY, blockSize, blockSize)
+}
+
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+}
